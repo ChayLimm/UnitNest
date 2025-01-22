14fe84f69eb9e0e033b0d85f0f7ff0bfa7a2e565
ChayLimm <chengchhaylim@gmail.com>
2025-01-22 16:25:11 +0700

    update component

--- lib/utils/component.dart
+++ lib/utils/component.dart
@@ -5,3 +5,10 @@ Color yellow = const Color(0xFFF8A849);
 Color green = const Color(0xFF4FAC80);
 Color grey = const Color(0xFF757575);
 Color blue = const Color(0xFF4A90E2);
+Color darkGrey = const Color(0xFF6F6F6F);
+Color white =  Colors.white;
+
+int h1 = 22;
+int h2 = 18;
+int p1 = 12;
+int p2 =10;
