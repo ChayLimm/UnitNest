073805ec6afc0799c04f0250c1ee0e1e3e22e317
ChayLimm <chengchhaylim@gmail.com>
2025-01-31 10:18:26 +0700

    finish setting up firebase with Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,26 @@
-import 'package:emonitor/model/building/building.dart';
-import 'package:emonitor/model/building/room.dart';
-import 'package:emonitor/model/system/priceCharge.dart';
-import 'package:emonitor/model/system/system.dart';
+
 import 'package:emonitor/screen/authentication/authenPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  // Ensures Firebase is initialized before the app starts
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBvgGhEXQnt-ERUqs-iyyV8kmLuVmXAg5E",
+      authDomain: "unitnest-f12cc.firebaseapp.com",
+      projectId: "unitnest-f12cc",
+      storageBucket: "unitnest-f12cc.appspot.com", // ✅ Fixed this line
+      messagingSenderId: "143712922998",
+      appId: "1:143712922998:web:49174d57789c20300f1e57",
+      measurementId: "G-WZX6BRVPWJ",
+    ),
+  ); // Initialize Firebase
 
-   var building1 =
-  Building(
-    name: "B1", 
-    address: "Phnom Penh",
-  )
-;
-
-building1.roomList.add(Room(name: "A001", price: 120));
-
-var pricecharge =
-  PriceCharge(
-    electricityPrice: 1, 
-    waterPrice: 1, 
-    hygieneFee: 1, 
-    finePerDay: 1, 
-    fineStartOn: 5, 
-    rentParkingPrice: 10, 
-    startDate: DateTime.now());
-
-
-System system1 = System(
-  id: "testing1",
-);
-system1.priceChargeList.add(pricecharge);
-system1.listBuilding.add(building1);
-
-print(system1.toJson());
-    WidgetsFlutterBinding.ensureInitialized();  // Ensures that Firebase is initialized before the app starts
-    await Firebase.initializeApp();  // Initialize Firebase
-      runApp(const MainApp());
-
+  runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
